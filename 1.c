654e00975ad4399859060cc6d122c53c7d16efd0
Gorbach <43406162+WereBudLUL@users.noreply.github.com>
2019-01-21 22:43:28 +0100

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,23 +1,61 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main()
 {
     
-    int input[100] = {0};
-    int i = 0;
-    printf("Bitte geben Sie Zahlen ein (max 100):");
+    void randnum(int * array11)
+    {
+    srand(time(0));
     
-    while(1)
+        for(int i=0;i<25;i++)
     {
- 
-    scanf("%d",&input[i]);
-    i++;
-    for(int j=0;j<100;j++)
+        
+        array11[i] = rand() % 100;
+    }
+    
+    for(int f = 0;f<25;f++)
     {
-        printf("%d",input[j]);
+        printf("%d ",array11[f]);
     }
     }
     
+    void reverse(int * array22)
+    {
+    int cache = 0;
     
+    int j = 0;
+    
+    for(int h = 24;h >= 12;h--&&j++)
+    {
+        cache = array22[h];
+        array22[h] = array22[j];
+        array22[j] = cache;
+        
+    }
+    printf("\nZahlen umgedreht:\n");
+    for(int u = 0;u<25;u++)
+    {
+        printf("%d ",array22[u]);
+    }
+    }
+    
+    int array[25];
+    int array2[25];
+
+
+    randnum(array);
+    printf("\n");
+    
+    
+    for(int g = 0;g<25;g++)
+    {
+        array2[g] = array[g];
+    }
+    
+    reverse(array2);
+    
+
     return 0;
 }
