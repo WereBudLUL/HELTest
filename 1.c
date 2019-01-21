e0eb14c8fcb28c4584d864f4404bd18ef206a502
Gorbach <43406162+WereBudLUL@users.noreply.github.com>
2019-01-21 22:45:32 +0100

    Update 1.c

--- 1.c
+++ 1.c
@@ -2,60 +2,64 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main()
+
+
+void randnum (int *array11)
 {
-    
-    void randnum(int * array11)
+  srand (time (0));
+
+  for (int i = 0; i < 25; i++)
     {
-    srand(time(0));
-    
-        for(int i=0;i<25;i++)
+
+      array11[i] = rand () % 100;
+    }
+
+  for (int f = 0; f < 25; f++)
     {
-        
-        array11[i] = rand() % 100;
+      printf ("%d ", array11[f]);
     }
-    
-    for(int f = 0;f<25;f++)
-    {
-        printf("%d ",array11[f]);
-    }
-    }
-    
-    void reverse(int * array22)
-    {
-    int cache = 0;
-    
-    int j = 0;
-    
-    for(int h = 24;h >= 12;h--&&j++)
-    {
-        cache = array22[h];
-        array22[h] = array22[j];
-        array22[j] = cache;
-        
-    }
-    printf("\nZahlen umgedreht:\n");
-    for(int u = 0;u<25;u++)
-    {
-        printf("%d ",array22[u]);
-    }
-    }
-    
-    int array[25];
-    int array2[25];
+}
 
 
-    randnum(array);
-    printf("\n");
-    
-    
-    for(int g = 0;g<25;g++)
-    {
-        array2[g] = array[g];
-    }
-    
-    reverse(array2);
-    
+void reverse (int *array22)
+{
+  int cache = 0;
 
-    return 0;
+  int j = 0;
+
+  for (int h = 24; h >= 12; h-- && j++)
+    {
+      cache = array22[h];
+      array22[h] = array22[j];
+      array22[j] = cache;
+
+    }
+  printf ("\nZahlen umgedreht:\n");
+  for (int u = 0; u < 25; u++)
+    {
+      printf ("%d ", array22[u]);
+    }
+}
+
+int main ()
+{
+
+
+  int array[25];
+  int array2[25];
+
+
+  randnum (array);
+  printf ("\n");
+
+
+  for (int g = 0; g < 25; g++)
+    {
+      array2[g] = array[g];
+    }
+
+  reverse (array2);
+
+
+  return 0;
 }
